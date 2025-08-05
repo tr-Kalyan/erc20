dc700b38293d05cc6ac2318c59410ba9b6518cdb
tr-Kalyan <kalyansde1@gmail.com>
2025-08-05 20:50:42 +0530

    minor bug fixes and race condition while approve allowance

--- src/ERC20.sol
+++ src/ERC20.sol
@@ -1,22 +1,21 @@
 // SPDX-License-Identifier: MIT
-pragma solidty ^0.8.20;
+pragma solidity ^0.8.20;
 
 interface IERC20 {
     event Transfer(address indexed from, address indexed to, uint256 value);
-    event approval(address indexed owner, address indexed spender, uint256 value);
-
+    event Approval(address indexed owner, address indexed spender, uint256 value);
     function totalSupply() external view returns (uint256);
     function balanceOf(address account) external view returns (uint256);
     function transfer(address to, uint256 value) external returns (bool);
-    function allowance(address owner,address spender) external view returns (uint256);
-    function approve(address spender, uint256 amount) external  returns (bool);
-    function transferFrom(address from, address to, uint256 value) external returns(bool);
+    function allowance(address owner, address spender) external view returns (uint256);
+    function approve(address spender, uint256 amount) external returns (bool);
+    function transferFrom(address from, address to, uint256 value) external returns (bool);
 }
 
 interface IERC20Metadata {
     function name() external view returns (string memory);
     function symbol() external view returns (string memory);
-    function decimals() external view returns(uint8);
+    function decimals() external view returns (uint8);
 }
 
 interface IERC20Errors {
@@ -31,44 +30,57 @@ interface IERC20Errors {
 contract ERC20 is IERC20, IERC20Metadata, IERC20Errors {
     string private _name;
     string private _symbol;
-    string private _decimals;
+    uint8 private _decimals;
     uint256 private _totalSupply;
-    mapping(address account=>uint256) private balances;
-    mapping(address account=> mapping(address spender=> uint256)) private _allowance;
+    mapping(address => uint256) private _balances;
+    mapping(address => mapping(address => uint256)) private _allowances;
 
-    constructor(string memory _name, string memory _symbol, uint8 _decimals) {
-        _name = _name;
-        _symbol = _symbol;
-        _decimals = _decimals;
-    }
-
-    modifier checkAllowance(address approver, address from, uint256 _balanceToDeduct) {
-        require(_allowance[approver][from] >= _balanceToDeduct, ERC20InsufficientAllowance(from, _allowance[approver][from], _balanceToDeduct));
-    }
-
-    modifier invalidApprover(address approver) {
-        require(approver != address(0), ERC20InvalidApprover(approver));
-      _;
-    }
-
-    modifier invalidSpender(address spender) {
-        require(spender != address(0), ERC20InvalidSpender(spender));
-      _;
-    }
-
-    modifier checkBalanceBeforeDeduction(address from, uint256 _balanceToDeduct) {
-        require(balances[from] >= _balanceToDeduct, ERC20InsufficientBalance(from, balances[from], _balanceToDeduct));
-      _;
+    constructor(string memory name_, string memory symbol_, uint8 decimals_) {
+        _name = name_;
+        _symbol = symbol_;
+        _decimals = decimals_;
     }
 
     modifier invalidSender(address sender) {
-        require(sender != address(0), ERC20InvalidSender(sender));
-      _;
+        if (sender == address(0)) {
+            revert ERC20InvalidSender(sender);
+        }
+        _;
     }
 
     modifier invalidReceiver(address receiver) {
-        require(receiver != address(0), ERC20InvalidReceiver(receiver));
-      _;
+        if (receiver == address(0)) {
+            revert ERC20InvalidReceiver(receiver);
+        }
+        _;
+    }
+
+    modifier invalidApprover(address approver) {
+        if (approver == address(0)) {
+            revert ERC20InvalidApprover(approver);
+        }
+        _;
+    }
+
+    modifier invalidSpender(address spender) {
+        if (spender == address(0)) {
+            revert ERC20InvalidSpender(spender);
+        }
+        _;
+    }
+
+    modifier checkBalanceBeforeDeduction(address account, uint256 amount) {
+        if (_balances[account] < amount) {
+            revert ERC20InsufficientBalance(account, _balances[account], amount);
+        }
+        _;
+    }
+
+    modifier checkAllowance(address owner, address spender, uint256 amount) {
+        if (_allowances[owner][spender] < amount) {
+            revert ERC20InsufficientAllowance(spender, _allowances[owner][spender], amount);
+        }
+        _;
     }
 
     function name() external virtual view returns (string memory) {
@@ -78,7 +90,7 @@ contract ERC20 is IERC20, IERC20Metadata, IERC20Errors {
     function symbol() external virtual view returns (string memory) {
         return _symbol;
     }
-    
+
     function decimals() external virtual view returns (uint8) {
         return _decimals;
     }
@@ -88,78 +100,78 @@ contract ERC20 is IERC20, IERC20Metadata, IERC20Errors {
     }
 
     function balanceOf(address account) external virtual view returns (uint256) {
-        return balances[account];
+        return _balances[account];
     }
 
     function transfer(address to, uint256 value) external virtual returns (bool) {
         address sender = msg.sender;
         _deductToken(sender, value);
         _addToken(to, value);
-
         emit Transfer(sender, to, value);
         return true;
     }
 
-
     function allowance(address owner, address spender) external virtual view returns (uint256) {
-        return _allowance[owner][spender];
+        return _allowances[owner][spender];
     }
 
-    function approve(address spender, uint256 value) external virtual returns(bool) {
-        address _personApproving = msg.sender;
-        _addAllowance(spender, _personApproving, value);
-
-        emit Approval(_personApproving, spender, value);
+    function approve(address spender, uint256 value) external virtual returns (bool) {
+        address owner = msg.sender;
+        _approve(owner, spender, value);
         return true;
     }
 
-
-    function transferFrom(address from, address to, uint256 value) external virtual returns(bool) {
+    function transferFrom(address from, address to, uint256 value) external virtual returns (bool) {
         address spender = msg.sender;
-        _deductAllowance(spender, from, value);
+        _spendAllowance(from, spender, value);
         _deductToken(from, value);
         _addToken(to, value);
-
         emit Transfer(from, to, value);
         return true;
     }
 
-    function _addAllowance(address from, address approver, uint256 value) internal invalidApprover(approver) invalidSpender(from) {
-        unchecked {
-            _allowance[approver][from] +=value;
+    function increaseAllowance(address spender, uint256 addedValue) public virtual returns (bool) {
+        address owner = msg.sender;
+        _approve(owner, spender, _allowances[owner][spender] + addedValue);
+        return true;
+    }
+
+    function decreaseAllowance(address spender, uint256 subtractedValue) public virtual returns (bool) {
+        address owner = msg.sender;
+        uint256 currentAllowance = _allowances[owner][spender];
+        if (currentAllowance < subtractedValue) {
+            revert ERC20InsufficientAllowance(spender, currentAllowance, subtractedValue);
         }
+        _approve(owner, spender, currentAllowance - subtractedValue);
+        return true;
     }
 
-    function _deductAllowance(address from, address approver,uint25 value) internal invalidApprover(approver) invalidSpender(from) checkAllowance(approver, from, value)  {
-        unchecked {
-            _allowance[approver][from] -= value;
-        }
+    function _approve(address owner, address spender, uint256 value) internal virtual invalidApprover(owner) invalidSpender(spender) {
+        _allowances[owner][spender] = value;
+        emit Approval(owner, spender, value);
     }
 
-    function _deductToken(address from, uint256 amount) internal invalidSender(from) checkBalanceBeforeDeduction(from, amount) {
-        unchecked {
-            balances[from] -= amount;
-        }
+    function _spendAllowance(address owner, address spender, uint256 value) internal virtual checkAllowance(owner, spender, value) {
+        _allowances[owner][spender] -= value;
     }
 
-    function _addToken(address to, uint256 amount) internal invalidReceiver(to) {
-      unchecked {
-        balances[to] += amount;
-      }
+    function _deductToken(address from, uint256 amount) internal virtual invalidSender(from) checkBalanceBeforeDeduction(from, amount) {
+        _balances[from] -= amount;
     }
 
-    function _mint(address to, uint256 amount) internal invalidReceiver(to) {
-     unchecked {
-      _totalSupply += amount;
-      balances[to] += amount;
-     }
+    function _addToken(address to, uint256 amount) internal virtual invalidReceiver(to) {
+        _balances[to] += amount;
     }
 
-    function _burn(address from, uint256 amount) internal checkBalanceBeforeDeduction(from, amount) invalidSender(from) {
-     unchecked {
-      _totalSupply -= amount;
-      balances[from] -= amount;
-     }
+    function _mint(address to, uint256 amount) internal virtual invalidReceiver(to) {
+        _totalSupply += amount;
+        _balances[to] += amount;
+        emit Transfer(address(0), to, amount);
+    }
+
+    function _burn(address from, uint256 amount) internal virtual invalidSender(from) checkBalanceBeforeDeduction(from, amount) {
+        _balances[from] -= amount;
+        _totalSupply -= amount;
+        emit Transfer(from, address(0), amount);
     }
 }
-
